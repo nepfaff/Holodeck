0e293f623471beb164ef233ff395070beea9b490
nepfaff <nepfaff@mit.edu>
2025-05-27 19:33:22 -0400

    Make runnable

---
+++ download_data.sh
@@ -0,0 +1,4 @@
+python -m objathor.dataset.download_holodeck_base_data --version 2023_09_23
+python -m objathor.dataset.download_assets --version 2023_09_23
+python -m objathor.dataset.download_annotations --version 2023_09_23
+python -m objathor.dataset.download_features --version 2023_09_23
